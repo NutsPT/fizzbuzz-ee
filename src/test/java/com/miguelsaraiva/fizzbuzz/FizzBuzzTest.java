37a06af073ed404697e770beed75aa4b9a666d83
Miguel Saraiva Sampaio <miguelsaraiva81@gmail.com>
2015-07-25 19:32:19 +0100

    Refactor Testing
    Refactor Testing

--- src/test/java/com/miguelsaraiva/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/miguelsaraiva/fizzbuzz/FizzBuzzTest.java
@@ -1,6 +1,6 @@
 package com.miguelsaraiva.fizzbuzz;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -27,35 +27,35 @@ public class FizzBuzzTest {
     public void testSimpleInputForExpectedOutputStepOne()
     {
     	FizzBuzz.main(new String[]{"1","20"});
-        assertEquals("1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz", outContent.toString());
+        Assert.assertEquals("1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz", outContent.toString());
     }
     
     @Test
     public void testInvalidRange()
     {
     	FizzBuzz.main(new String[]{"20","1"});
-        assertEquals("Invalid input (end is bigger then start)", outContent.toString());
+    	Assert.assertEquals("Invalid input (end is bigger then start)", outContent.toString());
     }
     
     @Test
     public void testINegativeIncludedRange()
     {
     	FizzBuzz.main(new String[]{"-15","15"});
-        assertEquals("fizzbuzz -14 -13 fizz -11 buzz fizz -8 -7 fizz buzz -4 fizz -2 -1 fizzbuzz 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz", outContent.toString());
+    	Assert.assertEquals("fizzbuzz -14 -13 fizz -11 buzz fizz -8 -7 fizz buzz -4 fizz -2 -1 fizzbuzz 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz", outContent.toString());
     }
     
     @Test
     public void testInvalidInputType()
     {
     	FizzBuzz.main(new String[]{"A","1"});
-        assertEquals("Invalid input", outContent.toString());
+    	Assert.assertEquals("Invalid input", outContent.toString());
     }
     
     @Test
     public void testInvalidNumberOfArguments()
     {
     	FizzBuzz.main(new String[]{"1","2","3"});
-        assertEquals("Invalid number of arguments", outContent.toString());
+    	Assert.assertEquals("Invalid number of arguments", outContent.toString());
     }
 
 }
